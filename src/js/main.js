a828262f1137e4cb366392827c5b8081ffd122e1
cgalean0 <cngaleano56@gmail.com>
2024-03-05 01:11:51 -0300

    fix: bugs and add functions to events of clicks

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+const buttonFlip = document.querySelector('.flip-card')
+
+
+function holaBton(){
+    buttonFlip.addEventListener('click', ()=>{
+        console.log('hola mundo');
+    })
+}
+holaBton()
